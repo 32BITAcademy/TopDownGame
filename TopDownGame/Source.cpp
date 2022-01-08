3adbc956dca75ec89a9f01038e6489a798a473a4
JamesListener <jameslistener@gmail.com>
2022-01-08 06:55:53 +0300

    Настроил свойства проекта (для SFML), добавил тестовое окно.

--- TopDownGame/Source.cpp
+++ TopDownGame/Source.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
+#include<SFML\Graphics.hpp>
+
+using namespace sf;
 
 int main()
 {
-	;
+    RenderWindow window(VideoMode(200, 200), "SFML works!");
+    CircleShape shape(100.f, 6);
+    shape.setFillColor(Color::Green);
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
