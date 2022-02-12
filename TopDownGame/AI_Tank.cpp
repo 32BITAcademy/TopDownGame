cc7ac9f074adb023d83051ad32bcb7564b2ccd72
32BITAcademy <32bitacademy@gmail.com>
2022-02-12 17:22:34 +0300

    Поправил класс AI_Tank

--- TopDownGame/AI_Tank.cpp
+++ TopDownGame/AI_Tank.cpp
@@ -1,17 +1,15 @@
 #include "AI_Tank.h"
 #include <iostream>
+
 using namespace sf;
-#define LEFT 20
-#define RIGHT 15
-#define UP 5
-#define DOWN 10
 using namespace std;
 
-AI_Tank::AI_Tank(): Unit(50.f, { 0,0,150,165 }, 100.f)
+enum Direction { NONE, RIGHT, DOWN, LEFT, UP };
+
+AI_Tank::AI_Tank(): time_left_to_move(0), Unit(50.f, { 400, 150 ,150,165 }, 100.f)
 {
 	InitAnimation("AI_Tank1", { 0,0,150,165 });
 }
-
 
 AI_Tank::~AI_Tank()
 {
@@ -19,30 +17,22 @@ AI_Tank::~AI_Tank()
 
 void AI_Tank::Update(sf::Time dt)
 {
-	Vector2f s = { 0.0f, 0.0f };
-	int chosen_dir;
-	float maxtime, _time;
-	srand(time(nullptr));
-	chosen_dir = rand() % 20;
-	maxtime = rand() % 2;
-	for (_time = 0.f; _time <= maxtime; )
+	if (time_left_to_move <= 0)
 	{
-		if (chosen_dir <= UP) s.y += -1.f;
-		else if (chosen_dir <= DOWN) s.y += 1.f;
-		else if (chosen_dir <= RIGHT) s.x += 1.f;
-		else if (chosen_dir <= LEFT) s.x += -1.f;
-		float speedlen = sqrt(s.x * s.x + s.y * s.y);
-		if (speedlen != 0)
+		time_left_to_move = rand() % 2501 + 500;
+		Direction chosen_dir = Direction(rand() % 5);
+		switch (chosen_dir)
 		{
-			s.x *= maxspeed / speedlen;
-			s.y *= maxspeed / speedlen;
+		case RIGHT: speed = { maxspeed, 0.f }; break;
+		case DOWN: speed = { 0.f, maxspeed }; break;
+		case LEFT: speed = { -maxspeed, 0.f }; break;
+		case UP: speed = { 0.f, -maxspeed }; break;
+		case NONE: speed = { 0.f, 0.f }; break;
 		}
-		speed = s;
-		_time += dt.asSeconds();
 	}
+	
+	time_left_to_move -= dt.asMilliseconds();
 	Unit::Update(dt);
-	
-	
 }
 
 void AI_Tank::SendMsg(MSG& m)
